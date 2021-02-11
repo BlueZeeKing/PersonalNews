38965685058cdb0f9142a6e5780c6803534c1c23
BlueZeeKing <braydenzee+git@gmail.com>
2021-02-10 19:55:10 -0500

    finished front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header data={props.data.header} />
+      <Body segments={props.data.body} />
     </div>
   );
+}
+
+function Header(props) {
+  return (
+    <div>
+      <MainTitle data={props.data.maintitle} />
+      <Info data={props.data.info} />
+    </div>
+  );
+}
+
+function MainTitle(props) {
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold font-sherif text-center mt-6">{props.data.title}</h1>
+      <h3 className="text-xl text-gray-600 text-center mb-2">{props.data.tagline}</h3>
+    </div>
+  );
+}
+
+function Info(props) {
+  return (
+    <div className="border-t border-b overflow-auto">
+      <h4 className="text-lg text-gray-800 float-left m-1 border-gray-200">{props.data.date}</h4>
+      <Weather data={props.data.weather}/>
+    </div>
+  );
+}
+
+function Weather(props) {
+  return (
+    <div className="float-right overflow-auto">
+      <h4 className="float-right m-1">{props.data.temp}</h4>
+      <img className="float-left m-1 h-7" alt="weather icon" src={props.data.img} />
+    </div>
+  )
+}
+
+function Body(props) {
+  let elements = []
+  for (let i = 0; i < props.segments.length; i++) {
+    elements.push(<Segment url={props.segments[i].url} title={props.segments[i].title} summary={props.segments[i].summary} key={'s'+i.toString()}/>)
+    elements.push(<hr className="mx-4 border-gray-200" key={'r'+i.toString()} />)
+  }
+
+  elements.pop()
+
+  return (
+    <div>
+      {elements}
+    </div>
+  )
+}
+
+function Segment(props) {
+  return (
+    <a href={props.url} className="m-2 block">
+      <h3 className="m-1 text-xl font-bold">{props.title}</h3>
+      <hr className="mx-8 border-gray-100"></hr>
+      <p className="m-1">{props.summary}</p>
+    </a>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const testData = {
+  header: {
+    maintitle: {
+      title: 'BZ news',
+      tagline: 'Noli esse stultus'
+    },
+    info: {
+      date: 'Wed Feb 10',
+      weather: {
+        temp: '10º',
+        img: null
+      }
+    }
+  },
+  body: [
+    {
+      title: 'test1',
+      summary: 'test1 description',
+      url: 'https://google.com'
+    },
+    {
+      title: 'test2',
+      summary: 'test2 description',
+      url: 'https://bing.com'
+    }
+  ]
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={testData}/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('main')
 );
 
 // If you want to start measuring performance in your app, pass a function
